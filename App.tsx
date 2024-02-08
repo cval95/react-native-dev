315e19ac0456a42f5cae70436fb7cf84f1eda11d
cval95 <cormacvalentine@outlook.com>
2024-02-08 16:30:24 +0000

    day-list

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import DayListItem from './src/components/core/DayListItem';
+
+const days = [...Array(24)].map((val, index) => index + 1);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>test</Text>
+    <View style={styles.container}> 
+
+    <FlatList 
+      data={days}
+      contentContainerStyle={styles.content}
+      columnWrapperStyle = {styles.column}
+      numColumns={2}
+      renderItem={({item}) => <DayListItem day={item}/>}
+    />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +26,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 10,
   },
+  content:{
+    gap: 10,
+  },
+  column:{
+    gap: 10,
+  }
 });
